af106eff76a3126af7606cc841c683c1f54facef
Ali Kücükcan <alikuecuekcan@gmail.com>
2023-08-24 22:46:30 +0200

    comment lines added

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,20 +4,26 @@ const UserModel = require("../models/user.model");
 
 // /user/<username> -> get user profile
 userRouter.get("/:username", async (req, res) => {
+  // which user wants to get from database ,
+  // username reads from url, username is a parameter. we declared on url as /:username,
+  // there for we can reach the username data from req.params.username
   let username = req.params.username;
   try {
+    // get the user by username from database
     let user = await UserModel.findOne({
       username: username,
     });
-    console.log("user", user);
+    // if user not exist
     if (!user) {
       res.status(404).json({
         error: "User Not Found",
       });
     } else {
+      // if user exist
       res.json(user);
     }
   } catch (error) {
+    // if there is an error returns an error
     res.status(500).json({
       error: "Server error",
     });
@@ -43,20 +49,24 @@ userRouter.delete("/me", authMiddleware, async (req, res) => {
 
 // if login reqiured we use auth middleware.
 userRouter.post("/update", authMiddleware, async (req, res) => {
+  // updated data from request body sent from frontend
   const newUserData = req.body;
+  // current user (who send request) provided from authMiddleware
   const user = req.user;
   try {
+    // update the current user with newUserData (sent from client)
     await UserModel.updateOne(
       {
         username: user.username,
       },
       newUserData
     );
-
+    // if succes
     res.json({
       message: "user updated",
     });
   } catch (error) {
+    // if failed
     res.status(500).json({
       error: "user not updated",
     });
